ef065b294e9302452200463229b90cf5f1b50787
mythili annamalai <mythiliannamalai45@gmail.com>
2022-03-12 18:05:12 +0530

    vowel

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿    class Program
+    {
+        public void Calulation()
+        {
+            char Alphabet;
+            Console.WriteLine("enter the alphabet : ");
+            Alphabet = char.Parse(Console.ReadLine());
+            if (Alphabet == 'a' || Alphabet == 'e' || Alphabet == 'i' || Alphabet == 'o' || Alphabet == 'u' || Alphabet == 'A' || Alphabet == 'E' || Alphabet == 'I' || Alphabet == 'O' || Alphabet == 'U')
+            {
+                Console.WriteLine("{0} is vowel...", Alphabet);
+
+            }
+            else
+            {
+                Console.WriteLine("{0} is consonant...", Alphabet);
+            }
+
+        }
+    }
+class Result
+{
+    public static void Main(string[]args)
+    {
+        Program vowel=new Program();
+        vowel.Calulation();
+    }
+}
